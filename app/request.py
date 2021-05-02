cc38f819dffd30ad3fbcb6e81bb8ae9a16f460cb
Bernice Twili <bernicetwili0@gmail.com>
2021-05-02 23:02:05 +0300

    revised appearance

--- app/request.py
+++ app/request.py
@@ -1,6 +1,7 @@
 import urllib.request,json
 from .models import  Article,Category, Source , Headlines
 
+
 # Getting api key
 api_key = None
 # Getting source url
@@ -9,10 +10,11 @@ source_url= None
 cat_url= None
 
 def configure_request(app):
-    global api_key, source_url, cat_url
+    global api_key,source_url,cat_url
     api_key = app.config['NEWS_API_KEY']
-    source_url= app.config['NEWS_API_SOURCE_URL']
-    cat_url=app.config['CAT_API_URL']
+    source_url =app.config['NEWS_API_SOURCE_URL']
+    cat_url = app.config['CAT_API_URL']
+
     
 def get_source():
     '''
--- tests/test_category.py
+++ tests/test_category.py
@@ -17,3 +17,5 @@ class CategoryTest(unittest.TestCase):
         Test to check creation of new Category instance,(is True)
         '''
         self.assertTrue(isinstance(self.new_category,Category))
+        
+# isinstance:::: Return true if the object argument is an instance of the classinfo argument, or of a (direct, indirect or virtual) subclass thereof. If object is not an object of the given type, the function always returns false.
